e7053ee590e36c3c5cc99d1b3b81084d674a1e21
mattykie <mattykie27@gmail.com>
2026-04-16 23:51:45 +0800

    pecpec

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, '.', '')
+
     return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.')
+            }
         }
-      }
-    };
-});
+    }
+})
